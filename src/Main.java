a194d5e409515a94f3fce2f21185e8f856422cfa
Darya Vylomova <qwerty6108@gmail.com>
2023-01-26 20:40:13 +0300

    initial commit

--- src/Main.java
+++ src/Main.java
@@ -1,15 +1,15 @@
 public class Main {
     public static void main(String[] args) {
 
-        int a = 50;
-        int p = 1000;
-        int b = p / 100;
+        int primordial = 50;
+        int added = 1000;
+        int bonus = added / 100;
 
-        if (p >= 1000) {
-        System.out.println("Итоговый счет " + (a + p + b));
-        System.out.println("Количество бонусных рублей составило " + b );
+        if (added >= 1000) {
+            System.out.println("Итоговый счет " + (primordial + added + bonus));
+            System.out.println("Количество бонусных рублей составило " + bonus);
         } else {
-            System.out.println("Итоговый счет " + (a + p));
+            System.out.println("Итоговый счет " + (primordial + added));
         }
     }
 }
